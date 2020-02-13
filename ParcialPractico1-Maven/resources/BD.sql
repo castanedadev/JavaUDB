7fccabf51720da0003717e6bc8f6eaf6203206fe
Rick C <rickcastaneda@outlook.com>
2020-02-13 17:11:53 -0600

    adding sql for table

--- ParcialPractico1-Maven/resources/BD.sql
+++ ParcialPractico1-Maven/resources/BD.sql
@@ -35,3 +35,50 @@ SELECT `Id`, `Nombres`, `Apellidos`, `Dui`, `Nit`, `SalarioBase`, `TipoEmpleado`
 FROM `empleados` WHERE `Dui` = '04610484-2'
 
 SELECT * FROM `empleados` WHERE `Dui` = '04610484-2'
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Feb 13, 2020 at 11:10 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `pp1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombres` varchar(100) DEFAULT NULL,
+  `Apellidos` varchar(100) DEFAULT NULL,
+  `Dui` varchar(10) NOT NULL,
+  `Nit` varchar(17) NOT NULL,
+  `SalarioBase` decimal(15,0) NOT NULL,
+  `TipoEmpleado` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `empleados`
+--
+
+INSERT INTO `empleados` (`Id`, `Nombres`, `Apellidos`, `Dui`, `Nit`, `SalarioBase`, `TipoEmpleado`) VALUES
+(1, 'Ricardo Jose', 'Castaneda Hernandez', '04610484-2', '0501-060492-101-1', '3500', 'TIPO_PROGRAMADOR'),
+(2, 'Simon Alberto', 'Parras Albiol', '00172855-2', '1023-051077-101-2', '900', 'TIPO_OTROS');
+COMMIT;
